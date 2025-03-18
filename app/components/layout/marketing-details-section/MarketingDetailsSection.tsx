537502784c365cf2afbfd9a99bab04c73a5b9dcf
Devan Rivera <devanrivera98@gmail.com>
2025-03-18 15:25:01 -0700

    mobile view of marketing details completed

---
+++ app/components/layout/marketing-details-section/MarketingDetailsSection.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image"
+import './marketing-details-section.css'
+
+interface MarketingCard {
+  icon: string;
+  alt: string;
+  title: string;
+  passage: string;
+}
+
+const MarketingCardArr: MarketingCard[] = [
+ {
+  icon: '/man-presenting.svg',
+  alt: 'figure presenting',
+  title: 'Dedicated Training and Development Specialist',
+  passage: 'From inital converstations and info sharing right through the training process and ultimately grand opening, Great Harvest team members are by your side through it all.'
+ },
+ {
+  icon: './mini-bakery.svg',
+  alt: 'mini bakery',
+  title: 'Hub and Spoke Bakery Café models',
+  passage: 'Whether you are interested in one store, the standard hub and two spokes, or building a bigger Great Harvest empire, our team is ready to get you setup for success with proven methods and processes that will help ensure a smooth entry into the Great Harvest franchise world.'
+ },
+ {
+  icon: './lightbulb.svg',
+    alt: 'lightbulb',
+  title: 'Marketing Strategies',
+  passage: 'Our marketing pros will help guide you through all phases of marketing. From hyper local to nationwide branding, we will be there to drive business to your Great Harvest!'
+ }
+]
+
+export default function MarketingDetailsSection() {
+  return (
+    <>
+      <section className="bg-brandBlue text-white">
+        <div className="flex flex-col marketing-section w-full">
+          <div className="flex justify-center w-full py-8">
+            <Image
+              src='/images/mobile-watering-cans.png'
+              alt="two watering cans"
+              width={312}
+              height={149}
+            />
+          </div>
+          <div className="px-5">
+            <h1 className="text-[48px] text-center">Let&apos;s Make BREAD together!</h1>
+          </div>
+          <div className="px-6 pt-6.5 text-[28px] flex justify-center text-center">
+            <p>Our team is dedicated to getting you open quickly and efficiently</p>
+          </div>
+          <div className="pb-10">
+            <ul>
+            {MarketingCardArr.map((marketingCard, index) => (
+              <li key={index} className='pt-12'>
+                <div className="flex justify-center">
+                  <Image src={marketingCard.icon} alt={marketingCard.alt} width={61} height={61} />
+                </div>
+                <h1 className="text-[20px] px-10 py-4 text-center">{marketingCard.title}</h1>
+                <div className="px-5">
+                  <p className="text-[18px]">{marketingCard.passage}</p>
+                </div>
+              </li>
+            ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
