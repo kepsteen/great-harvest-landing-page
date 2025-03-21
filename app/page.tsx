121c7911122fd9bd6aed49dde84562fe41f9e991
Cody Epstein <codyepstein@gmail.com>
2025-03-21 11:10:30 -0700

    add faq section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import TestimonialsSection from "@/app/components/layout/testimonials-section";
 import MarketingDetailsSection from "./components/layout/marketing-details-section/MarketingDetailsSection";
 import Footer from "./components/footer/Footer";
 import FranchiseStepsSection from "./components/layout/franchise-steps-section";
+import FAQSection from "./components/layout/faq-section";
+
 export default function Home() {
 	return (
 		<div className="flex flex-col relative min-h-screen">
@@ -14,6 +16,7 @@ export default function Home() {
 			<MarketingDetailsSection />
 			<TestimonialsSection />
 			<FranchiseStepsSection />
+			<FAQSection />
 			<Footer />
 		</div>
 	);
