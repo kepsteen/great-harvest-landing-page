0b6c2735f81a2b144e9eb9c3862a23a7ee944c22
Devan Rivera <devanrivera98@gmail.com>
2025-03-25 18:02:22 -0700

    horizontal padding removed on navbar and desktop landing page edits completed

--- app/landing/LandingPage.tsx
+++ app/landing/LandingPage.tsx
@@ -14,7 +14,7 @@ export default function LandingPage() {
 				<div
 					className="landing-section-container"
 				>
-					<div className="mx-auto lg:mx-0 my-auto w-full lg:w-1/2 flex flex-col gap-y-10 md:gap-y-26">
+					<div className="mx-auto lg:mx-0 my-auto w-full lg:w-1/2 flex flex-col gap-y-10 md:gap-y-15">
 						<div className="flex flex-col justify-center text-center">
 							<h1 className="text-[74px] font-bold text-cream text-shadow leading-20 title-text-shadow">
 								Harvest
@@ -26,8 +26,8 @@ export default function LandingPage() {
 								Today
 							</h1>
 						</div>
-						<div className="w-full px-1 pb-5 md:px-24">
-							<div className="px-3 md:px-5 py-1 md:py-5 rounded-xl bg-lightBeigeOp text-brandBlue font-bold text-xl">
+						<div className="w-full px-1 md:px-24">
+							<div className="px-3 md:px-5 py-1 md:py-5 rounded-xl bg-lightBeigeOp text-brandBlue font-bold text-xl md:mx-5">
 								<h1>Revolutionary Concept</h1>
 								<span>
 									Since 1976 Great Harvest has been
