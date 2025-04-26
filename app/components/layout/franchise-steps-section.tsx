33fe1fb5d95f4e93ed6c3bc677b68a13d03838b9
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-04-26 09:09:59 -0600

    feat: mobile version of process

--- app/components/layout/franchise-steps-section.tsx
+++ app/components/layout/franchise-steps-section.tsx
@@ -1,65 +1,90 @@
 import Image from "next/image";
 
-const steps = [
-	"Submit your interest below",
-	"Introductory call",
-	"Continue your due diligence",
-	"Experience Great Harvest Day - Meet the Team",
-	"Sign a Franchise Agreement and get ready for training",
-];
-
 export default function FranchiseStepsSection() {
 	return (
-		<>
-			<div
-				id="theProcess"
-				className="relative w-full bg-gradient-to-b from-bgTan from-0% via-bgTan via-10% to-cream to-20% border-t border-textGray"
-			>
-				<section className="py-16 w-full max-w-full mx-auto">
-					<div className="container mx-auto px-4 mb-12">
-						<div className="text-center mb-12">
-							<h2 className="text-brandBlue text-5xl mb-5">
-								Winning with Great Harvest
-							</h2>
-							<p className="text-[1.75rem] text-textGray">
-								Let us guide you through the next
-								steps!
-							</p>
-						</div>
-
-						<div className="flex flex-col xl:flex-row xl:gap-8 xl:items-start">
-							<div className="mx-auto px-4 w-full xl:flex-1 max-w-full sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-none">
-								{steps.map((step, index) => (
-									<div
-										key={index}
-										className="flex items-center mb-8"
-									>
-										<div className="flex-shrink-0 w-20 h-20 bg-bgTan border border-textGray rounded-full flex items-center justify-center mr-6">
-											<span className="text-5xl text-textGray">
-												{index + 1}
-											</span>
-										</div>
-										<div className="text-[1.75rem] text-brandBlue">
-											{step}
-										</div>
-									</div>
-								))}
-							</div>
-
-							<div className="mt-10 xl:mt-0 xl:flex-1 flex justify-center px-4 sm:px-6 lg:px-8">
-								<div className="relative w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-none h-80 xl:h-[500px]">
-									<Image
-										src="/images/catering-tray-1.jpg"
-										alt="Great Harvest Food"
-										fill
-										className="object-cover rounded-lg"
-									/>
-								</div>
-							</div>
-						</div>
+		<section
+			id="theProcess"
+			className="relative w-full bg-brand-blue"
+		>
+			<div className="py-16 w-full max-w-full mx-auto">
+				<div className="container mx-auto px-4 max-w-[342px] text-brand-blue">
+					<div className="text-center bg-white shadow-[0px_4px_4px_0px_#00000040] p-[10px]">
+						<h2 className="text-[48px] mb-[20px]">
+							Our Revolutionary Process
+						</h2>
+						<p className="text-[20px]">
+							Since 1976, Great Harvest has set
+							the industry standard by providing
+							fresh-baked breads, goodies and more
+							using only Montana-grown wheat.
+						</p>
 					</div>
-				</section>
+					<div className="flex flex-col bg-white shadow-[4px_4px_4px_0px_#00000040] rounded-[20px] p-[20px] mt-[20px]">
+						<ul className="flex flex-col gap-[15px]">
+							<li className="flex flex-row items-start gap-[15px]">
+								<Image
+									src="/icons/mixing-bowl.svg"
+									alt=""
+									height={40}
+									width={40}
+								/>
+								<div>
+									<h3 className="text-[28px] mb-[10px]">
+										Week 1-2
+									</h3>
+									<p className="text-[18px]">
+										Discovery call and FDD review
+									</p>
+								</div>
+							</li>
+							<li className="flex flex-row items-start gap-[15px]">
+								<Image
+									src="/icons/oven.svg"
+									alt=""
+									height={40}
+									width={40}
+								/>
+								<div className="p-0">
+									<h3 className="text-[28px] p-0">
+										Week 3-4
+									</h3>
+									<p className="text-[18px]">
+										Application
+									</p>
+								</div>
+							</li>
+							<li className="flex flex-row items-start gap-[15px]">
+								<Image
+									src="/icons/bread-loaf.svg"
+									alt=""
+									height={40}
+									width={40}
+								/>
+								<div>
+									<h3 className="text-[28px]">
+										Months 2-3
+									</h3>
+									<p className="text-[18px]">
+										Franchise Agreement, Site
+										Selection and Development
+									</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<Image
+						src="/images/southlake-interior.png"
+						alt="Great Harvest worker wearing a dark t-shirt prepares an item"
+						width={0}
+						height={0}
+						style={{
+							width: "100%",
+							height: "auto",
+						}}
+						className="shadow-[4px_4px_4px_0px_#00000040] mt-[30px]"
+					/>
+				</div>
 			</div>
-		</>
+		</section>
 	);
 }
