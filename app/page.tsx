03300314444fd3fd54f48ab629cd7cb60639002a
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-04-25 19:03:47 -0600

    a11y: move nav and footer inline with main instead of being inside of main element

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./landing/LandingPage";
 import FranchiseMetricsSection from "@/app/components/layout/franchise-metrics-section";
 import TestimonialsSection from "@/app/components/layout/testimonials-section";
 import MarketingDetailsSection from "./components/layout/marketing-details-section/MarketingDetailsSection";
-import Footer from "./components/footer/Footer";
 import FranchiseStepsSection from "./components/layout/franchise-steps-section";
 import FAQSection from "./components/layout/faq-section";
 import ContactSection2 from "./components/layout/contact-section-2/contact-section-2";
@@ -12,8 +10,6 @@ import ContactSection from "./components/layout/contact-section";
 export default function Home() {
 	return (
 		<div className="flex flex-col relative min-h-screen">
-			<Navbar />
-			{/* // Todo: Move Navbar to the root layout*/}
 			<LandingPage />
 			<ContactSection />
 			<FranchiseMetricsSection />
@@ -22,7 +18,6 @@ export default function Home() {
 			<FranchiseStepsSection />
 			<FAQSection />
 			<ContactSection2 />
-			<Footer />
 		</div>
 	);
 }
