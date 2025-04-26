50112af1d7dfc6886e839205071d5a82b1b0cb82
Devan Rivera <devanrivera98@gmail.com>
2025-04-25 22:25:21 -0700

    mobile map section completed

--- app/components/layout/great-harvest-map-section/AvailabilityMapSection.tsx
+++ app/components/layout/great-harvest-map-section/AvailabilityMapSection.tsx
@@ -3,16 +3,30 @@ import Image from "next/image"
 export default function AvailabilityMapSection() {
   return (
     <>
-    <section className="bg-wheat-extra-light flex gap-y-7.5">
-      <div className="py-7.5">
+    <section className="bg-wheat-extra-light  w-full">
+      <div className="flex flex-col gap-y-7.5 py-7.5">
         <div>
           <h1 className="text-[48px] font-medium text-center text-brand-blue drop-shadow-lg pb-[20px]">Ripe for Harvesting</h1>
           <h2 className="text-[20px] px-6 text-center">
             Which neighborhood will you share Great Harvest with?
           </h2>
         </div>
-        <div>
-        <Image src="/images/map-great-harvest.png" width={350} height={220} alt="available Great Harvest locations" />
+        <div className="w-full">
+        <Image className="mx-auto" src="/images/map-great-harvest.png" width={350} height={220} alt="available Great Harvest locations" />
+        </div>
+        <div className="mx-auto grid gap-y-2.5">
+          <div className="flex">
+            <div className="w-7 h-7 rounded-full bg-wheat-medium mr-2.5"></div>
+            <p className="text-lg">High Availibality</p>
+          </div>
+          <div className="flex">
+            <div className="w-7 h-7 rounded-full bg-light-tan mr-2.5"></div>
+            <p className="text-lg">Mixed Availibality</p>
+          </div>
+          <div className="flex">
+            <div className="w-7 h-7 rounded-full bg-wheat-dark mr-2.5"></div>
+            <p className="text-lg">Low Availibality</p>
+          </div>
         </div>
       </div>
     </section>
