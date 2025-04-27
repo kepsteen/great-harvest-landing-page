35363820bf8c6e1da2ca7c7a41818d6882bf8146
Devan Rivera <devanrivera98@gmail.com>
2025-04-26 17:42:13 -0700

    desktop layout of testimonial created

--- app/components/layout/testimonial-section/TestimonialSection.tsx
+++ app/components/layout/testimonial-section/TestimonialSection.tsx
@@ -5,19 +5,19 @@ export default function TestimonialSection() {
     <>
       <section className="bg-[url(https://imgc.allpostersimages.com/img/posters/karandaev-wood-texture-cutting-board-background_u-l-q1052q40.jpg?artHeight=550&artPerspective=n&artWidth=550&background=ffffff)] pt-10 pb-5">
         <div>
-          <h1 className="font-normal text-[48px] text-center">Backed by our Franchisees</h1>
-          <div className="bg-white rounded-xl mx-5 px-6 pt-6">
-            <div className="mb-2.5">
-            <p className="text-[13.5px]">
-              “Owning a Great Harvest bakery has been nothing short of transformational—for our family, our team, and our community. What started as a dream to build something meaningful has become a deeply rooted part of our daily lives. The opportunity to blend hand-crafted food with heart-centered service has given us purpose beyond just running a business.  ... The Great Harvest model has given us the tools and freedom to create a bakery that reflects our values—where kindness matters, quality is never compromised, and people genuinely feel at home. We&rsquo;ve built lasting relationships with customers, partnered with local organizations, and created jobs that have helped people grow not just professionally, but personally.”
-            </p>
-            <div className="py-2.5">
-              <h2 className="text-end">Elise, Katie, & Kodiak Smith</h2>
-              <p className="text-end">Harvesting in<br />Northwest, Indiana</p>
+          <h1 className="font-normal text-[48px] text-center py-4">Backed by our Franchisees</h1>
+          <div className="bg-white rounded-xl mx-5 px-6 pt-6 md:pb-6 md:grid md:grid-cols-2 md:w-[80%] md:mx-auto">
+            <div className="mb-2.5 md:order-2 lg:my-auto">
+              <p className="text-[13.5px] leading-[17px] md:pr-6 md:pl-5">
+                “Owning a Great Harvest bakery has been nothing short of transformational—for our family, our team, and our community. What started as a dream to build something meaningful has become a deeply rooted part of our daily lives. The opportunity to blend hand-crafted food with heart-centered service has given us purpose beyond just running a business.  ... The Great Harvest model has given us the tools and freedom to create a bakery that reflects our values—where kindness matters, quality is never compromised, and people genuinely feel at home. We&rsquo;ve built lasting relationships with customers, partnered with local organizations, and created jobs that have helped people grow not just professionally, but personally.”
+              </p>
+              <div className="py-2.5 md:pb-0">
+                <h2 className="text-end md:text-start md:pl-5">Elise, Katie, & Kodiak Smith</h2>
+                <p className="text-end md:text-start md:pl-5">Harvesting in<br />Northwest, Indiana</p>
+              </div>
             </div>
-            </div>
-            <div className="pb-4">
-              <Image className="rounded-full mx-auto" src="/images/franchisees-owner.png" width={290} height={298} alt="Three of Great Harvest franchise owner in Great Harvest apparel" />
+            <div className="pb-4 md:pb-0 md:h-full md:order-1 md:flex md:items-center">
+              <Image className="lg:w-full max-w-[475px] rounded-full mx-auto" src="/images/franchisees-owner.png" width={290} height={298} alt="Three of Great Harvest franchise owner in Great Harvest apparel" />
             </div>
           </div>
         </div>
