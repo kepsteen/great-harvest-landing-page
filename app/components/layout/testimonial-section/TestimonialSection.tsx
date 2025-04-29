f2891f458b5596d733b75183e09815e4d138172f
Devan Rivera <devanrivera98@gmail.com>
2025-04-29 13:16:34 -0700

    updated testimonial for approved design

--- app/components/layout/testimonial-section/TestimonialSection.tsx
+++ app/components/layout/testimonial-section/TestimonialSection.tsx
@@ -4,40 +4,17 @@ export default function TestimonialSection() {
 	return (
 		<>
 			<section
-				className="bg-[url(https://imgc.allpostersimages.com/img/posters/karandaev-wood-texture-cutting-board-background_u-l-q1052q40.jpg?artHeight=550&artPerspective=n&artWidth=550&background=ffffff)] pt-10 pb-5 md:py-12.5"
+				className="bg-brand-blue pt-10 pb-5 md:py-12.5"
 				id="testimonial"
 			>
 				<div>
-					<h2 className="text-[48px] text-center py-4 md:pt-0">
+					<h2 className="text-[48px] text-center text-white py-4 md:pt-0">
 						Backed by our Franchisees
 					</h2>
-					<div className="bg-white rounded-xl mx-5 px-6 pt-6 md:pb-6 md:grid md:grid-cols-2 md:w-[80%] md:mx-auto">
+					<div className="bg-white rounded-xl mx-5 px-6 md:px-9 pt-6 md:pb-6 md:grid md:grid-cols-[35%_65%] md:mx-[84px] place-self-center lg:w-3/5">
 						<div className="mb-2.5 md:order-2 lg:my-auto">
-							<p className="text-[13.5px] leading-[17px] md:pr-6 md:pl-5">
-								“Owning a Great Harvest bakery has
-								been nothing short of
-								transformational—for our family,
-								our team, and our community. What
-								started as a dream to build
-								something meaningful has become a
-								deeply rooted part of our daily
-								lives. The opportunity to blend
-								hand-crafted food with
-								heart-centered service has given
-								us purpose beyond just running a
-								business. ... The Great Harvest
-								model has given us the tools and
-								freedom to create a bakery that
-								reflects our values—where kindness
-								matters, quality is never
-								compromised, and people genuinely
-								feel at home. We&rsquo;ve built
-								lasting relationships with
-								customers, partnered with local
-								organizations, and created jobs
-								that have helped people grow not
-								just professionally, but
-								personally.”
+							<p className="text-[13.6px] leading-[17px] md:pr-0 md:pl-5">
+								“Owning a Great Harvest bakery has been nothing short of transformational—for our family, our team, and our community. What started as a dream to build something meaningful has become a deeply rooted part of our daily lives. The opportunity to blend hand-crafted food with heart-centered service has given us purpose beyond just running a business.  ... The Great Harvest model has given us the tools and freedom to create a bakery that reflects our values—where kindness matters, quality is never compromised, and people genuinely feel at home. We&rsquo;ve built lasting relationships with customers, partnered with local organizations, and created jobs that have helped people grow not just professionally, but personally.”
 							</p>
 							<div className="py-2.5 md:pb-0">
 								<p className="text-end md:text-start md:pl-5 font-ghPlain">
@@ -50,12 +27,12 @@ export default function TestimonialSection() {
 								</p>
 							</div>
 						</div>
-						<div className="pb-4 md:pb-0 md:h-full md:order-1 md:flex md:items-center">
+						<div className="pb-4 md:pb-0 md:ml-5 md:h-full md:order-1 md:flex md:items-center">
 							<Image
-								className="lg:w-full max-w-[475px] rounded-full mx-auto"
+								className="max-w-[475px] rounded-full mx-auto"
 								src="/images/franchisees-owner.png"
-								width={290}
-								height={298}
+								width={162}
+								height={162}
 								alt="Three Great Harvest franchise owners in Great Harvest apparel"
 							/>
 						</div>
