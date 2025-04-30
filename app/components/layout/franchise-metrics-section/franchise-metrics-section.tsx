b407cbb42c1f047510dad88fcf6e903cc104ceb9
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-04-30 08:05:24 -0600

    fix: set max width on cards container

--- app/components/layout/franchise-metrics-section/franchise-metrics-section.tsx
+++ app/components/layout/franchise-metrics-section/franchise-metrics-section.tsx
@@ -20,7 +20,7 @@ export default function FranchiseMetricsSection() {
 					</p>
 				</div>
 
-				<section className="flex flex-col gap-4 p-4 md:grid md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+				<section className="flex flex-col gap-4 p-4 md:grid md:grid-cols-2 md:gap-8 lg:grid-cols-3 max-w-[1080px] mx-auto">
 					{metricCardData.map((card, index) => (
 						<MetricCard
 							key={card.id}
